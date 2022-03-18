8e7359212746a217aba619637cefd7a1a5726602
Bruno Santos <bruno_st17@hotmail.com>
2022-03-17 21:39:28 -0300

    Home page open

--- src/test/java/modulos/produtos/ProdutoTest.java
+++ src/test/java/modulos/produtos/ProdutoTest.java
@@ -3,6 +3,7 @@ package modulos.produtos;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -17,7 +18,17 @@ public class ProdutoTest {
         browser.manage().window().maximize();
 
         browser.get("http://165.227.93.41/lojinha-web/v2/");
-        
+
+        browser.findElement(By.cssSelector("label[for='usuario']")).click();
+        browser.findElement(By.id("usuario")).sendKeys("brunonoptqs");
+
+        browser.findElement(By.cssSelector("label[for='senha']")).click();
+        browser.findElement(By.id("senha")).sendKeys("123456");
+
+        browser.findElement(By.cssSelector("button[type=\"submit\"]")).click();
+
+
+
 
 
     }
